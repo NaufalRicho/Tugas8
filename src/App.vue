5053e9cda5c02a05d8109137fb07e2b291c28412
naufalriko28 <naufalriko28@gmail.com>
2023-06-06 20:30:14 +0700

    frist commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import Navbar from './Navbar.vue'; 
+  import Hero from './Hero.vue';
+</script>
+
+<template>
+  <header>
+    <Navbar/>
+  </header>
+
+  <main>
+    <Hero/>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="hero">
+      <div class="container-fluid py-6">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5 px-5">
+          <div class="col-10 col-sm-8 col-lg-6">
+             <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_sk5h1kfn.json" background="transparent" speed="1"
+                        style="width: 100%; height: auto;" loop autoplay></lottie-player>
+          </div>
+          <div class="col-lg-6">
+            
+            <h1 class="display-5 fw-bold lh-1 mb-3">Belajar Bareng - Online Platform</h1>
+            <p class="lead">BelajarKuy</p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+              <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Sign In</button>
+              <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
